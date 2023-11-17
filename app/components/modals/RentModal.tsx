634e7a6ba5697c408a5d6572422e388f1ec671f2
Konstantin Ivanovic <121982642+Konstantin907@users.noreply.github.com>
2023-11-17 11:10:13 -0800

    img uplad done

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -14,6 +14,7 @@ import { FieldValues, useForm } from "react-hook-form";
 import CountrySelect from "../inputs/CountrySelect";
 import dynamic from "next/dynamic"
 import Counter from "../inputs/Counter";
+import ImageUpload from "../inputs/ImageUpload";
 
 //sve ovew korake pravimo posebno:
 enum STEPS {
@@ -61,6 +62,7 @@ export default function RentModal() {
     const guestCount = watch("guestCount");
     const roomCount = watch("roomCount");
     const bathroomCount = watch("bathroomCount");
+    const imageSrc = watch("imageSrc");
 
 //ovako se mapa importuje umjesto standardnog:
 //svaki put se mapa rerenderuje nakon promjene lokacije
@@ -185,6 +187,22 @@ if(step === STEPS.INFO) {
     )
     
 }
+
+    //img body cointent next step for STEPS:
+    if (step === STEPS.IMAGES) {
+        bodyContent = (
+          <div className="flex flex-col gap-8">
+            <Heading
+              title="Add a photo of your place"
+              subtitle="Show guests what your place looks like!"
+            />
+            <ImageUpload
+              onChange={(value) => setCustomValue('imageSrc', value)}
+              value={imageSrc}
+            />
+          </div>
+        )
+      }
 
     return (
     <div>
